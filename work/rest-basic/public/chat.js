ab6a4b99416ddd6a8f1ef720f72ea27d0aa827e5
sarahlighter <sgjybm@outlool.com>
2019-02-28 21:10:45 -0800

    update

--- work/rest-basic/public/chat.js
+++ work/rest-basic/public/chat.js
@@ -1,31 +1,22 @@
 (function intervalRequest(){
-	setInterval(()=>{
-		updateMessagesAndUsers();
-	},5000);
-	sendCheck();
-	loginCheck();
-	removeRefreshButton();
-	sendMessage();
-})();
 
-function updateMessagesAndUsers(){
-	fetch('/messages/')
-	.then( response =>{
-		if( response.ok ) {
-			return response.json();
-		}else{
-			console.log(response.json());
-			throw new Error("Couldn't get data!");
-		}
-	})
-	.then( response =>{
-		updateMessages( response.messages );
-		updateUsers( response.users );
-	});
-}
+	const updateMessagesAndUsers = () =>{
+		fetch('/messages/')
+		.then( response =>{
+			if( response.ok ) {
+				return response.json();
+			}else{
+				throw new Error("Couldn't get data!");
+			}
+		})
+		.then( response =>{
+			updateMessages( response.messages );
+			updateUsers( response.users );
+		});
+	};
 
-function updateMessages( messages ){
-	const allMessage = messages.map( message => 
+	const updateMessages = ( messages ) =>{
+		const allMessage = messages.map( message => 
 		`<li>
 		    <div class="message">
 		        <div class="meta-info">
@@ -39,102 +30,125 @@ function updateMessages( messages ){
 		    	<p class="message-text">${message.text}</p>
 		    </div>
 		</li>`).join('');
-	document.querySelector('.messages').innerHTML = allMessage;
-}
+		const messagesDIV = document.querySelector('.messages');
+		if(messagesDIV){
+			messagesDIV.innerHTML = allMessage;
+		}
+	};
 
-function updateUsers( users ){
-	const allUser = Object.values(users).map( user => 
+	const updateUsers = ( users ) =>{
+		const allUser = Object.values(users).map( user => 
 		`<li>
 			<div class="user">
 			    <span class="username">${user}</span>
 			</div>
 		</li>
-	`).join('');
-	document.querySelector('.users').innerHTML = allUser;
-}
-
-function sendCheck() {
-	const sendButton = document.querySelector(".send button");
-	const toSend = document.querySelector(".to-send");
-	if(toSend && sendButton) {
-	   	sendButton.disabled = !toSend.value;
-	    toSend.addEventListener('input', (e) => {
-	    if(e.target.value) {
-	    	sendButton.disabled = false;	    	
-	    } else {
-	        sendButton.disabled = true;
-	    }
-	    });
-	}
-}
-
-function loginCheck(){
-	if(document.querySelector(".login")){
-		const login = document.querySelector(".login").children[0];
-		const username = login.children[0];
-		const loginButton = login.children[1];
-		if(username && loginButton){
-			loginButton.disabled = !username.value;
-			username.addEventListener('input',(e) =>{
-				if(e.target.value) {
-					loginButton.disabled = false;
-				}else{
-					loginButton.disabled = true;
-
-				}
-			});
+		`).join('');
+		const usersDIV = document.querySelector('.users');
+		if(usersDIV){
+			usersDIV.innerHTML = allUser;
 		}
-	}
-}
+	};
 
-function removeRefreshButton(){
-	const refreshButton = document.querySelector(".refresh");
-	if(refreshButton)
-		refreshButton.parentNode.removeChild(refreshButton);
-}
-
-function sendMessage(){
-	const sendButton = document.querySelector(".send button");
-	if(!sendButton) return;
-	sendButton.addEventListener('click', () => {
-		disableSendFormPost();
-		const currentURL = new URL(document.URL);
-		const username = currentURL.searchParams.get("username");
-		const text = document.querySelector(".to-send").value;
-		if(username && text){
-			document.querySelector(".to-send").value = '';
-			fetch('/messages/', {
-				method: 'POST',
-				headers: new Headers({      
-					'content-type': 'application/json'    
-				}), 
-				body: JSON.stringify( {username, text} )
-			})
-			.then( response => {
-				if( response.ok ){
-					updateMessagesAndUsers();
-					resetErrorStatus();
-				} else {
-					throw new Error("Your post fetch is failed");
-				}
-			})
-			.catch( err => showErrorStatus(err));
+	const sendCheck = () =>{
+		const sendButton = document.querySelector(".send button");
+		const toSend = document.querySelector(".to-send");
+		if(toSend && sendButton) {
+		   	sendButton.disabled = !toSend.value;
+		    toSend.addEventListener('input', (e) => {
+			    if(e.target.value) {
+			    	sendButton.disabled = false;	    	
+			    } else {
+			        sendButton.disabled = true;
+			    }
+		    });
 		}
-	});
-}
+	};
 
-function disableSendFormPost(){
-	//prevent send form from posting to /chat
-	const sendForm = document.querySelector(".send");
-	sendForm.action="";
-	sendForm.method="";
-}
+	const loginCheck = () =>{
+		if(document.querySelector(".login")){
+			const login = document.querySelector(".login").children[0];
+			const username = login.children[0];
+			const loginButton = login.children[1];
+			if(username && loginButton){
+				loginButton.disabled = !username.value;
+				username.addEventListener('input',(e) =>{
+					if(e.target.value) {
+						loginButton.disabled = false;
+					}else{
+						loginButton.disabled = true;
 
-function showErrorStatus( ResponseStatus ){
-	document.querySelector(".show-errors p").innerText=ResponseStatus;
-}
+					}
+				});
+			}
+		}
+	};
 
-function resetErrorStatus(){
-	document.querySelector(".show-errors p").innerText='';
-}
+	const removeRefreshButton = () =>{
+		const refreshButton = document.querySelector(".refresh");
+		if(refreshButton)
+			refreshButton.parentNode.removeChild(refreshButton);
+
+	};
+
+	// document.querySelector(".send button").addEventListener('click', () => {
+
+	// }
+
+	const sendMessage = () =>{
+		const sendButton = document.querySelector(".send button");
+		if(!sendButton) return;
+		sendButton.addEventListener('click', () => {
+			const currentURL = new URL(document.URL);
+			const username = currentURL.searchParams.get("username");
+			const text = document.querySelector(".to-send").value;
+			if(username && text){
+				document.querySelector(".to-send").value = '';
+				fetch('/messages/', {
+					method: 'POST',
+					headers: new Headers({      
+						'content-type': 'application/json'    
+					}), 
+					body: JSON.stringify( {username, text} )
+				})
+				.then( response => {
+					if( response.ok ){
+						updateMessagesAndUsers();
+						resetErrorStatus();
+					} else {
+						throw new Error("Your post fetch is failed");
+					}
+				})
+				.catch( err => showErrorStatus(err));
+			}
+		});
+	};
+
+	const disableSendFormPost = () =>{
+		//prevent send form from posting to /chat
+		const sendForm = document.querySelector(".send");
+		if(!sendForm) return;
+		sendForm.removeAttribute("action");
+		sendForm.removeAttribute("method");
+		const sendButton = document.querySelector(".send button");
+		sendButton.removeAttribute("type");
+	};
+
+	const showErrorStatus = ( ResponseStatus ) =>{
+		document.querySelector(".show-errors p").innerText=ResponseStatus;
+	};
+
+	const resetErrorStatus = () =>{
+		document.querySelector(".show-errors p").innerText='';
+	};
+
+	setInterval(()=>{
+		updateMessagesAndUsers();
+	},5000);
+	sendCheck();
+	loginCheck();
+	removeRefreshButton();
+	//sendMessage();
+	disableSendFormPost();
+})();
 

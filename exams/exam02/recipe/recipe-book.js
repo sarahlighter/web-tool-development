1dfa667871c23420920c40e8d660c0ab47abda20
sarahlighter <sgjybm@outlool.com>
2019-03-23 17:15:06 -0700

    delete preserved recipe data

--- exams/exam02/recipe/recipe-book.js
+++ exams/exam02/recipe/recipe-book.js
@@ -1,7 +1,4 @@
-const recipes = {
-	"cola chicken": {title:"cola chicken", ingredients: "cola, chicken wings, ginger", instructions:"1. boil the chicken wings, 2. put cola and ginger inside 3.vapourize it 4. done" },
-	"piculi": {title:"piculi", ingredients:"cucumber, vineger, laoganma",instructions:"cut cucumber ,put everything inside,done" },
-};
+const recipes = {};
 
 function addRecipe({title, ingredients, instructions}){
 	recipes[title] = {title,ingredients, instructions};
